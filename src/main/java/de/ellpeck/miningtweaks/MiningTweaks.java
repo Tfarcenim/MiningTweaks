00b94acfbd0cc2a84b3cf106af5077a0384c88d5
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2019-12-29 20:57:29 -0500

    1.15 port

--- src/main/java/de/ellpeck/miningtweaks/MiningTweaks.java
+++ src/main/java/de/ellpeck/miningtweaks/MiningTweaks.java
@@ -1,117 +1,87 @@
 package de.ellpeck.miningtweaks;
 
+import com.google.common.collect.Lists;
 import net.minecraft.block.Block;
 import net.minecraft.util.ResourceLocation;
-
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent.OnConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.GameData;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.apache.commons.lang3.tuple.Pair;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import java.util.List;
 
-@Mod(modid = MiningTweaks.MOD_ID, name = MiningTweaks.NAME, version = MiningTweaks.VERSION, guiFactory = "de.ellpeck.miningtweaks.GuiFactory")
-public class MiningTweaks{
+import static net.minecraftforge.common.MinecraftForge.EVENT_BUS;
 
-    public static final String MOD_ID = "miningtweaks";
-    public static final String NAME = "MiningTweaks";
-    public static final String VERSION = "@VERSION@";
+@Mod(MiningTweaks.MOD_ID)
+public class MiningTweaks {
 
-    public static final Logger LOGGER = LogManager.getLogger(NAME);
+  public static final String MOD_ID = "miningtweaks";
 
-    public static Configuration config;
-    private static String[] hardnessTweaks;
-    private static String[] miningLevelTweaks;
-    private static float hardnessMultiplier;
+  public MiningTweaks() {
+    ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, SERVER_SPEC);
+    FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onConfigurationChangedEvent);
+    EVENT_BUS.addListener(this::serverstart);
+  }
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event){
-        config = new Configuration(event.getSuggestedConfigurationFile());
-        config.load();
-        defineConfigs();
+  public static void handleConfig() {
+    ConfigHandler.tweak_hardness.get().forEach(s -> {
+      String[] split = s.split("@");
+      Block block = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(split[0]));
+      block.blockHardness = Float.parseFloat(split[1]);
+    });
 
-        MinecraftForge.EVENT_BUS.register(new ConfigEventHandler());
+    ConfigHandler.tweak_mining_level.get().forEach(s -> {
+      String[] split = s.split("@");
+      Block block = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(split[0]));
+      Util.setHarvestTool(block, ToolType.get(split[1]));
+      Util.setHarvestLevel(block, Integer.parseInt(split[2]));
+    });
+
+  }
+
+  public void serverstart(FMLServerStartedEvent e) {
+    handleConfig();
+  }
+
+  public void onConfigurationChangedEvent(OnConfigChangedEvent event) {
+    if (MOD_ID.equals(event.getModID())) {
+      handleConfig();
     }
+  }
 
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent event){
+  public static final ConfigHandler SERVER;
+  public static final ForgeConfigSpec SERVER_SPEC;
 
-        for(Block block : GameData.getBlockRegistry().typeSafeIterable()){
-            try{
-                float oldHardness = block.getBlockHardness(null, null, null);
+  static {
+    final Pair<ConfigHandler, ForgeConfigSpec> specPair2 = new ForgeConfigSpec.Builder().configure(ConfigHandler::new);
+    SERVER_SPEC = specPair2.getRight();
+    SERVER = specPair2.getLeft();
+  }
 
-                if(oldHardness >= 0f){
-                    float newHardness = oldHardness*hardnessMultiplier;
-                    block.setHardness(newHardness);
-                }
-            }
-            catch(Exception e){
-                LOGGER.error("Could not apply hardness multiplier to "+block.getUnlocalizedName()+"!", e);
-            }
-        }
+  public static class ConfigHandler {
 
-        for(String s : hardnessTweaks){
-            try{
-                String[] parts = s.split("@");
+    public static ForgeConfigSpec.ConfigValue<List<? extends String>> tweak_hardness;
 
-                String reg = parts[0];
-                float hardness = Float.parseFloat(parts[1]);
+    public static ForgeConfigSpec.ConfigValue<List<? extends String>> tweak_mining_level;
 
-                Block block = Block.REGISTRY.getObject(new ResourceLocation(reg));
-                block.setHardness(hardness);
-            }
-            catch(Exception e){
-                LOGGER.error("Trying to parse config "+s+" for hardness tweaks failed!", e);
-            }
-        }
+    public ConfigHandler(ForgeConfigSpec.Builder builder) {
+      builder.push("general");
+      tweak_hardness =
+              builder
+                      .comment("The blocks whose hardness should be modified. This needs to be the registry name of the block followed by an @ followed by the new hardness, so for example: 'minecraft:stone@5'")
+                      .defineList("tweak_hardness", Lists.newArrayList(), String.class::isInstance);
 
-        for(String s : miningLevelTweaks){
-            try{
-                String[] parts = s.split("@");
-
-                String reg = parts[0];
-                int meta = Integer.parseInt(parts[1]);
-                String tool = parts[2];
-                int level = Integer.parseInt(parts[3]);
-
-                Block block = Block.REGISTRY.getObject(new ResourceLocation(reg));
-
-                if(meta == 32767){
-                    block.setHarvestLevel(tool, level);
-                }
-                else{
-                    block.setHarvestLevel(tool, level, block.getStateFromMeta(meta));
-                }
-            }
-            catch(Exception e){
-                LOGGER.error("Trying to parse config "+s+" for mining level tweaks failed!", e);
-            }
-        }
+      tweak_mining_level =
+              builder
+                      .comment("The blocks whose mining level should be modified. This needs to be the registry name of the block followed by an @ followed by the tool class ('pickaxe', 'axe' or 'shovel') followed by an @ followed by the harvest level (0 = wood, 1 = stone, 2 = iron, 3 = diamond, custom levels work), so for example: 'minecraft:stone@pickaxe@2'")
+                      .defineList("tweak_mining_level", Lists.newArrayList(), String.class::isInstance);
+      builder.pop();
     }
-
-    private static void defineConfigs(){
-        hardnessTweaks = config.getStringList("hardness", Configuration.CATEGORY_GENERAL, new String[0], "The blocks whose hardness should be modified. This needs to be the registry name of the block followed by an @ followed by the new hardness, so for example: 'minecraft:stone@5'");
-        miningLevelTweaks = config.getStringList("mininglevels", Configuration.CATEGORY_GENERAL, new String[0], "The blocks whose mining level should be modified. This needs to be the registry name of the block followed by an @ followed by the metadata (32767 if it should apply to all ones) followed by an @ followed by the tool class ('pickaxe', 'axe' or 'shovel') followed by an @ followed by the harvest level (0 = wood, 1 = stone, 2 = iron, 3 = diamond, custom levels work), so for example: 'minecraft:stone@0@pickaxe@2'");
-        hardnessMultiplier = config.getFloat("hardnessmultiplier", "Multipliers", 1f, 0f, 100000f, "The hardness of all registered blocks will be multiplied by this value." + Configuration.NEW_LINE + "Note: This setting gets applied before the 'hardness' and 'mininglevels' settings are applied." + Configuration.NEW_LINE);
-
-        if(config.hasChanged()){
-            config.save();
-        }
-    }
-
-    private static class ConfigEventHandler{
-
-        @SubscribeEvent
-        public void onConfigurationChangedEvent(OnConfigChangedEvent event){
-            if(MOD_ID.equals(event.getModID())){
-                defineConfigs();
-            }
-        }
-    }
+  }
 }
